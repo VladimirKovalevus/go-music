d154f1e545abeab5cd9ecf0ab8968a5b687262a8
Syndafloden <vladkoval805@gmail.com>
2024-03-18 11:08:30 +0700

    ui interface

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,30 +1,44 @@
 package ui
 
 import (
+	"fmt"
+
 	"fyne.io/fyne"
 	"github.com/VladimirKovalevus/go-music/internal/core"
 	"github.com/VladimirKovalevus/go-music/internal/core/playback"
 )
 
 type UI struct {
+	Core            *core.Core
 	TrackList       fyne.CanvasObject
 	Playlists       fyne.CanvasObject
-	StopStart       fyne.CanvasObject
-	PreviousTrack   fyne.CanvasObject
-	NextTrack       fyne.CanvasObject
+	stopStart       fyne.CanvasObject
+	previousTrack   fyne.CanvasObject
+	nextTrack       fyne.CanvasObject
 	TrackInfo       fyne.CanvasObject
-	Core            core.Core
 	Playlist        []playback.Playlist
 	CurrentPlaylist uint32
 }
 
 func (u *UI) SelectPlaylist() {
 }
-
 func (u *UI) ReloadTracks() {
-
+}
+func (u *UI) NewPlayList(name string, icon []byte) {
+}
+func (u *UI) AddTrackToPlaylist(p *playback.Playlist, t *playback.Track) {
+}
+func (u *UI) NextTrack() {
+	fmt.Println(u.nextTrack)
+}
+func (u *UI) PreviousTrack() {
+	fmt.Println(u.previousTrack)
+}
+func (u *UI) StopStart() {
+	fmt.Println(u.stopStart)
 }
 
-func Init() UI {
+func NewUi() UI {
+	// widget.NewList()
 	return UI{}
 }
